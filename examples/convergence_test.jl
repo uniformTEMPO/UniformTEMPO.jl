baebd744fd453d55feac4a9dd375d9acaaa8967d
mgarbellini <matteo.garbellini@tu-dresden.de>
2026-07-27 15:21:11 +0200

    add example for `convergence` and `resume_from_checkpoint` functions

--- examples/convergence_test.jl
+++ examples/convergence_test.jl
@@ -1,99 +1,88 @@
+# usage of the UniformTEMPO built-in convergence routines
+
 using UniformTEMPO
 import SpecialFunctions.zeta, SpecialFunctions.gamma
-using LinearAlgebra
 using Plots
 
 include("../utils/convergence.jl");
 
+# model as in `examples/spin_boson_fdt.jl`
+begin
+    s = 1; ω_c = 5; α = 0.2; β = 1;
 
-s = 1; 
-ω_c = 5;
-α = 0.2;
+    bcf_0(t) = α / pi * gamma(s + 1) * (ω_c / (1 + im * ω_c * t))^(s + 1);
+    bcf_th(t) = 2 * α / (pi * β^(s + 1)) * gamma(s + 1) * real(zeta(s + 1, (1 + β * ω_c + im * ω_c * t) / (β * ω_c)));
+    bcf(t) = bcf_0(t) + bcf_th(t);
 
-β = 1;
-bcf_0(t) = α / pi * gamma(s + 1) * (ω_c / (1 + im * ω_c * t))^(s + 1);
-bcf_th(t) = 2 * α / (pi * β^(s + 1)) * gamma(s + 1) * real(zeta(s + 1, (1 + β * ω_c + im * ω_c * t) / (β * ω_c)));
-bcf(t) = bcf_0(t) + bcf_th(t);
+    S = [[0 1]; [1 0]];
+    H_sys = [[1 0]; [0 -1]];
+end;
+# FDT demands that Sₛ = 2 * (n_B.(ω_eval) .+ 1) .* imag.(χ) for a thermal bath,
+# with Sₛ the power spectral density, χ the susceptibility, and n_B the bose distribution 
+# We (convergence) check that the identity is satisfied in the frequency domain. 
 
-S = [[0 1]; [1 0]];
-H_sys = [[1 0]; [0 -1]];
+
 
 # convergence grid: svd accuracy and trotter time step 
-accuracy = 10 .^ LinRange(-5, -12, 4);
-trotter_steps = reverse(LinRange(0.01, 0.1, 4));
+accuracy = 10 .^ LinRange(-5, -12, 20);
+trotter_steps = reverse(LinRange(0.01, 0.1, 20));
 
-# Testing convergence routine
-# Argument structure is the same as computing the process tensor 
-# max_rank is an optional argument, but recommended 
-bdim, values, index = convergence(S, trotter_steps, bcf, accuracy; max_rank = 250) do MyPT 
-    
-    ω_eval= LinRange(0, 10, 2000)
-    susceptibility(MyPT, S, S, ω_eval; h_s = H_sys)
+# A label for the convergence test can be specified
+label = "fluctuation dissipation theorem";
 
+# A <path> and <filename> for the convergence run can be specified. Each unique convergence run creates a folder <path>/<filename>, a convergece file <path>/<filename>/<filename>.jld2
+path = pwd();
+filename = "fdt_spin_boson_α_$(α)_β_$(β)"; # with or w/out ".jld2" suffix
+
+# Additional metadata can be saved in dictionary form 
+metadata = Dict{String,Any}(
+    "computed_value"  => "Sₛ - 2 * (n_B(ω_eval) + 1) * imag(χ)",
+    "omega_range" => (-5, 5),
+    "n_omega"     => 1000,
+    "H_sys"       => string(H_sys));
+
+# The computed process tensor can be saved for each (trotter_step, accuracy) combination
+# False by default. If true, an additional folder is created at <path>/<filename>/pt_<filename>
+pt_save = true;
+
+# A maximum rank can be specified. This avoid running into memory issues whenever the bond dimension becomes very large for the (trotter_step, accuracy) combination. This option is *not* `cap_rank`.
+max_rank = 350;
+
+
+# The convergence function is called with a `do` block. 
+# It returns a 2d array of bond dimensions `bdim`, of `values`, and 1d array of the maximum `index` (of accuracy) reached for every trotter step before `max rank` was reached.
+
+# The arguments follow the same ordering in the `uniTEMPO(...) function`. 
+# Any additional uniTEMPO argument *must* be passed after the convergence specific arguments
+bdim, values, index = convergence(  S, trotter_steps, bcf, accuracy;
+                                    label, metadata, pt_save, filename, path, # convergence arguments
+                                    max_rank # uniTEMPO argument
+                                ) do MyPT
+    # quantity to evaluate     
+    ω_eval = LinRange(-5, 5, 1000)
+    χ = susceptibility(MyPT, S, S, -ω_eval; h_s = H_sys) 
+    Sₛ = 2 * real.(two_point_correlations_fourier(MyPT, S, S, -ω_eval; h_s = H_sys)) 
+    n_B(w) = 1 / (exp(β * w) - 1) 
+
+    # return fdt value
+    2 * (n_B.(ω_eval) .+ 1) .* imag.(χ) - Sₛ
 end;
 
+# In case the convergence run is terminated before its completion, it can be resumed from the checkpoint file
+bdim, values, index = resume_from_checkpoint(S, bcf; path, filename, pt_save = true) do MyPT
+    # quantity to evaluate     
+    ω_eval = LinRange(-5, 5, 1000)
+    χ = susceptibility(MyPT, S, S, -ω_eval; h_s = H_sys) 
+    Sₛ = 2 * real.(two_point_correlations_fourier(MyPT, S, S, -ω_eval; h_s = H_sys)) 
+    n_B(w) = 1 / (exp(β * w) - 1) 
 
-# The bond dimension can be visualized
-heatmap(something.(bdim, NaN), cmap = :heat, xlabel = "Accuracy", ylabel = "Trotter time step")
-χ, mcχ = maximum_common_χ(bdim, index; ref = 250);
-contour!(something.(bdim, NaN), c=:black, levels = [χ])
-
-
-# Convergence: bond dimension χ
-t = 8; trotter_steps[t]
-δε = Array{Float64}(undef, index[t]-1);
-δχ = similar(δε);
-
-for k in 1:(index[t]-1)
-    δε[k] = sum(abs.(values[t, k+1] .- values[t, k]))/length(values[t, k])
-    δχ[k] = abs(bdim[t, k+1] - bdim[t, k])
-end
-scatter(bdim[t, 1:(index[t]-1)], δε ./ δχ, lw = 3, yscale = :log10, xscale = :log10)
-
-# Convergence: trotter step Δ
-δϵ = Array{Float64}(undef, length(trotter_steps)-1);
-δΔ = similar(δϵ);
-χ, mcχ = maximum_common_χ(bdim, index; ref = 250, tol = 10);
-for t in eachindex(δϵ)
-    δϵ[t] = sum(abs.(values[t,mcχ[t]] .- values[t+1,mcχ[t+1]]))/length(values[t, mcχ[t]])
-    δΔ = abs(trotter_steps[t+1]-trotter_steps[t])
+    # return fdt value
+    2 * (n_B.(ω_eval) .+ 1) .* imag.(χ) - Sₛ
 end
 
-plot(trotter_steps[begin:end-1], δϵ ./ δΔ)
 
 
 
 
 
 
-
-
-# Bond dimension, fixed trotter step 
-t = 10
-colors = cgrad(:blues, index[t]-1, categorical=true);
-
-plt = plot(xlabel = "ω", ylabel = "diff")
-for k in 1:index[t]-1 
-    diff = values[t, index[t]] .- values[t, k]
-    plot!(plt, real.(diff), lw = 2.5, lc = colors[k], label = bdim[t, k])
-end
-plot!(plt)
-
-
-# Trotter step, comparing last computation
-plt = plot(xlabel = "ω", ylabel = "value")
-for t in eachindex(values[:, 1])
-    plot!(plt, imag.(values[t, index[t]-1]))
-end
-plot!(plt)
-
-
-
-
-#plotting the spectrum 
-MyPT = uniTEMPO(S, 0.05, bcf, 1e-8);
-bond_dim(MyPT)
-
-plotly();
-λ, v = spectrum(MyPT; h_s = H_sys);
-scatter(λ)
